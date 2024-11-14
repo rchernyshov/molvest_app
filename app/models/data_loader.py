b8b49f5c2bc923e7267e76a097e269ea2130b35a
Roman <tchernyshoff.roman2015@gmail.com>
2024-11-14 19:57:59 +0300

    fix start app

--- app/models/data_loader.py
+++ app/models/data_loader.py
@@ -1,10 +1,13 @@
 import pandas as pd
+from app.config import DATA_PATH
+
 
 class DataLoader:
     def load_data(self):
         # Загрузка данных и подготовка
-        data = pd.read_csv("data.csv", sep=";", encoding="cp1251")
-        data['column_name'] = data['column_name'].str.replace(',', '.').astype(float)
+        data = pd.read_csv(DATA_PATH, delimiter=';', header=0, names=['Date', 'Product_Category', 'Product', 'City', 'Customer_Group', 'Point_Format', 'Sales_kg'])
+
+        data['Sales_kg'] = data['Sales_kg'].str.replace(',', '.').astype(float)
         # Пример предобработки данных
-        data["Дата"] = pd.to_datetime(data["Дата"], format="%d.%m.%Y")
+        data["Date"] = pd.to_datetime(data["Date"], format="%d.%m.%Y")
         return data

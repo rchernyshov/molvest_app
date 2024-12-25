f27c4e14b8aa5fa606fbc7e47c6870367c57aa7e
Chernyshov Roman <129329752+rchernyshov@users.noreply.github.com>
2024-12-25 20:58:36 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,9 @@ from flask import jsonify, request
 from .model.prediction import PredictionModel
 from .model.data_loader import DataLoader
 
-# Загружаем и подготавливаем данные
 data_loader = DataLoader()
 data = data_loader.load_data()
 
-# Создаем экземпляр модели
 model = PredictionModel(data)
 model.train_model()
 
@@ -18,7 +16,6 @@ def setup_routes(app):
         prediction = model.predict(date_str)
         return jsonify(prediction)
 
-    # Новый маршрут для получения статистики
     @app.route("/data/summary", methods=["GET"])
     def summary_statistics():
         stats = model.get_summary_statistics()
@@ -36,13 +33,8 @@ def setup_routes(app):
 
     @app.route("/data/raw", methods=["GET"])
     def get_raw_data():
-
-        # Получаем данные в их исходном виде, без обработки
         raw_data = data_loader.load_data()
-
-        # Преобразуем данные в формат, который будет возвращен в ответе
         result = raw_data[["Date", "Sales_kg"]].to_dict(orient="records")
-
         return jsonify(result)
 
     @app.route("/predict/train_model", methods=["POST"])
@@ -55,9 +47,6 @@ def setup_routes(app):
 
     @app.route("/data/correlation", methods=["GET"])
     def get_correlation():
-        # Получаем корреляцию
         correlation_matrix = data_loader.calculate_correlation()
-
-        # Преобразуем корреляцию в словарь и возвращаем как JSON
         correlation_dict = correlation_matrix.to_dict()
         return jsonify(correlation_dict)
